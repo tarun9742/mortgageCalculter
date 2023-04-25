92797a26202b5b63b525d3fcb311b9d55fb52db7
Tarun soni <tsoni9742@gmail.com>
2023-04-25 14:19:21 +0530

    mortageCalculater

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function fun1(){
+function fun1() {
     let a = document.getElementById('inputField1').value;
     let b = document.getElementById('inputField2').value;
     let c = document.getElementById('inputField3').value;
@@ -6,24 +6,24 @@ function fun1(){
     let w = parseInt(a);
     let x = parseInt(b);
     let y = parseInt(c);
-    
-    
-    let z = w + (w * x * y) / 100  ;
-    
-    let z2 = z/12;
-    
-    let z3 = (w * x * y) / 100 ;
-    
-    
-    document.getElementById('mma').innerHTML = "Your Monthly EMI will be ₹" + z2.toFixed(2);
-document.getElementById('tpm').innerHTML = "Totle Amount you have to pay is ₹" + z.toFixed(2);
-document.getElementById('tim').innerHTML = "You will pay ₹" + z3.toFixed(2) + " as Interest ";
 
-document.getElementById("result").style.backgroundColor = "#54f7b7";
-   
+
+    let z = w + (w * x * y) / 100;
+
+    let z2 = z / 12;
+
+    let z3 = (w * x * y) / 100;
+
+
+    document.getElementById('mma').innerHTML = "Your Monthly EMI will be ₹" + z2.toFixed(2);
+    document.getElementById('tpm').innerHTML = "Totle Amount you have to pay is ₹" + z.toFixed(2);
+    document.getElementById('tim').innerHTML = "You will pay ₹" + z3.toFixed(2) + " as Interest ";
+
+    document.getElementById("result").style.backgroundColor = "#54f7b7";
+
 }
 
-function fun2(){
+function fun2() {
     document.getElementById('inputField1').value = " ";
     document.getElementById('inputField2').value = " ";
     document.getElementById('inputField3').value = " ";
